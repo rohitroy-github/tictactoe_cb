81b3e63db3bdbc0b2013547190288a033fecd830
Rohit Roy <rohitdevops.roy@gmail.com>
2021-03-02 21:39:02 +0530

    game developing

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import Board from './Board'
 
-export default function Game() {
+export default class Game extends React.Component {
+    constructor(){
+        super()
+        this.state={
+            xIsNext: true,
+            stepNumber:0,
+            history: [
+                {squares: Array(9).fill(null)}
+                //{squares: ['x',null,null]},
+                //{squares}
+            ]
+        }
+    }
+    render() 
+    {
     return(
         <div className="game">
             <div className="game-board">
                 <Board/>
             </div>
         </div>
-        
     )
+    }
 }
