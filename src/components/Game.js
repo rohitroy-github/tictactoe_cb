8b49c3445698a6ea66619a881c7aa846a7537e64
Rohit Roy <rohitdevops.roy@gmail.com>
2021-03-02 22:46:03 +0530

    new add

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,24 +2,38 @@ import React from 'react'
 import Board from './Board'
 
 export default class Game extends React.Component {
-    constructor(){
-        super()
+    constructor(props) {
+        super(props)
         this.state={
             xIsNext: true,
             stepNumber:0,
             history: [
                 {squares: Array(9).fill(null)}
-                //{squares: ['x',null,null]},
-                //{squares}
-            ]
+            ],
+            value: 5
         }
+    }
+    
+    changeState= () => {
+        this.setState(
+            {
+                value: 6
+            }
+        )
+    }
+
+    someFunction = () => {
+        return 5
     }
     render() 
     {
+        console.log(this.state)
+        const result = this.someFunction()
     return(
         <div className="game">
             <div className="game-board">
-                <Board/>
+                <Board value = {result}/>
+                <button onClick ={() => this.changeState()}>Click Me</button>
             </div>
         </div>
     )
